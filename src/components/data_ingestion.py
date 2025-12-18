7dc20971300639c5dee27bba077faa26bcfd2576
AKASHKATYAN <akashkatyan2020@gmail.com>
2025-12-18 18:53:15 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,11 +6,17 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+    train_data_path: str=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'artifacts',"train.csv")
+    test_data_path: str=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'artifacts',"test.csv")
+    raw_data_path: str=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -33,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -47,4 +53,8 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
